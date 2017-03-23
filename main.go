46976bfa0351d9114a1a1e028584df339292c907
Lars Gierth <larsg@systemli.org>
2017-03-23 05:52:16 +0100

    Scan orgs to update targets

--- main.go
+++ main.go
@@ -1,30 +1,114 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	gh "github.com/google/go-github/github"
+	oauth2 "golang.org/x/oauth2"
 )
+
+const pagesYml = ".ipfspages.yml"
 
 type PagesServer struct {
 	webhookSecret []byte
-	accessToken   string
+	github        *gh.Client
+	ymlBranch     string
 }
 
-func NewPagesServer(secret []byte, token string) *PagesServer {
-	ps := &PagesServer{webhookSecret: secret, accessToken: token}
+func NewPagesServer(secret []byte, github *gh.Client) *PagesServer {
+	ps := &PagesServer{
+		webhookSecret: secret,
+		github:        github,
+		ymlBranch:     "master",
+	}
 	return ps
+}
+
+func (ps *PagesServer) RefreshTargets(ctx context.Context, org string) error {
+	opt := &gh.RepositoryListByOrgOptions{ListOptions: gh.ListOptions{PerPage: 10}}
+	for {
+		repos, resp, err := ps.github.Repositories.ListByOrg(ctx, org, opt)
+		if err != nil {
+			return err
+		}
+		for _, repo := range repos {
+			err = ps.refreshTarget(ctx, repo)
+			if err != nil {
+				return err
+			}
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.ListOptions.Page = resp.NextPage
+	}
+	return nil
+}
+
+func (ps *PagesServer) refreshTarget(ctx context.Context, repo *gh.Repository) error {
+	owner := strings.Split(repo.GetFullName(), "/")[0]
+	name := repo.GetName()
+
+	ref, resp, err := ps.github.Git.GetRef(ctx, owner, name, "heads/"+ps.ymlBranch)
+	if resp.StatusCode == 404 {
+		ps.removeTarget(repo)
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	tree, _, err := ps.github.Git.GetTree(ctx, owner, name, ref.Object.GetSHA(), false)
+	if err != nil {
+		return err
+	}
+
+	var ymlentry *gh.TreeEntry
+	for _, entry := range tree.Entries {
+		if entry.GetPath() == pagesYml {
+			ymlentry = &entry
+			break
+		}
+	}
+	if ymlentry == nil {
+		ps.removeTarget(repo)
+		return nil
+	}
+
+	ymlblob, _, err := ps.github.Git.GetBlob(ctx, owner, name, ymlentry.GetSHA())
+	if err != nil {
+		return err
+	}
+
+	ps.addTarget(repo, ymlblob)
+
+	return nil
+}
+
+func (ps *PagesServer) addTarget(repo *gh.Repository, yml *gh.Blob) error {
+	log.Printf("addTarget: %s", repo.GetFullName())
+	return nil
+}
+
+func (ps *PagesServer) removeTarget(repo *gh.Repository) error {
+	log.Printf("removeTarget: %s", repo.GetFullName())
+	return nil
 }
 
 func (ps *PagesServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	whtype, event, err := ps.parseWebhook(r)
 	if err != nil {
-		errorResponse(w, r, err)
+		ps.errorResponse(w, r, err)
 		return
 	}
+
+	// - refreshTarget if on ymlBranch, and changed files includes .ipfspages.yml
+	// - build if on one of branches specified in .ipfspages.yml
 
 	switch event := event.(type) {
 	case *gh.PushEvent:
@@ -34,6 +118,12 @@ func (ps *PagesServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusAccepted)
+}
+
+func (ps *PagesServer) errorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("%s %s -- %s", r.Method, r.RequestURI, err)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error\n\n" + err.Error()))
 }
 
 func (ps *PagesServer) parseWebhook(r *http.Request) (string, interface{}, error) {
@@ -65,19 +155,21 @@ func main() {
 		log.Fatalf("argument must be URL, e.g. http://127.0.0.1:4242/webhook -- %s", err)
 	}
 
-	ps := NewPagesServer(webhookSecret, accessToken)
+	ctx := context.Background()
+	oat := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken})
+	oac := oauth2.NewClient(ctx, oat)
+	github := gh.NewClient(oac)
+
+	ps := NewPagesServer(webhookSecret, github)
+
+	for _, org := range []string{"ipfs", "ipld", "orbitdb", "libp2p", "multiformats"} {
+		err = ps.RefreshTargets(ctx, org)
+		if err != nil {
+			log.Fatalf("RefreshTargets: %s", err)
+		}
+	}
+
 	http.Handle(listen.Path, ps)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotFound)
-	})
-
 	log.Printf("Listening for webhooks on: %s", listen)
 	log.Fatal(http.ListenAndServe(listen.Host, nil))
-}
-
-func errorResponse(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("%s %s -- %s", r.Method, r.RequestURI, err)
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Write([]byte("500 Internal Server Error\n\n" + err.Error()))
 }
