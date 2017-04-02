f5f4ecaefaaf4ff76931a76d2aef51f8e7728595
Lars Gierth <larsg@systemli.org>
2017-04-02 03:48:06 +0200

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,155 @@
-# ipfs-pages
-Easy publishing of static web content on IPFS
+# ipfs.li
+
+- https://ipfs.li
+- entrypoint for project knowledge+activity
+- indexes github, pinbot, irc, mailing list, twitter, meetup, sprint, ipfs-community calendar, waffle, incidents, etc. activity
+  - imports files, attachments, gists, hackmd, zoom
+  - first steps: github issues, irc lines
+  - latency is hidden in the indexer, instead of presented to the UI
+- ultrafast results
+  - ipfs.li/#searchterm, and redirct from searchterm.ipfs.li
+  - finger tree
+    - great because we can very quickly provide recent results, and then load more older results
+    - how fast is finger tree on ipfs generally?
+    - https://github.com/krl/aolog
+  - fast autofocus on search form
+    - don't render search with js
+    - just plain onload=focusInput, onsubmit=donothing, then when ipfs.js is loaded give feedback and do search
+  - dweb.link and cache-control: immutable
+  - windows.ipfs and polyfill
+  - time to first byte
+  - load js-ipfs node on first edit, until then just use dweb.link gateway (or local if rewritten by addon)
+- daily/weekly digest posting or email
+- browse by date, type, author, top-k words
+
+# docs.ipfs.io
+
+- Home
+- The IPFS stack
+  - IPFS
+    - cli, api, fs-repo, datastore, bitswap, dag-pb / merkledag, unixfs, dnslink, fs path / dweb link, ipns, gateway
+  - IPLD
+  - libp2p
+  - Multiformats
+- Implementations
+  - go-ipfs
+  - js-ipfs
+- Usage
+  - Installing, updating
+  - HTTP-to-IPFS gateway
+  - Pinning
+  - In web browsers
+  - With large datasets
+  - Naming / IPNS
+    - multiple names
+    - republishing
+  - Pubsub
+  - Running in production
+    - Repo migrations
+    - Garbage collection
+    - Monitoring
+    - Failure modes
+    - Analyzing performance metrics
+    - Debugging
+    - Containers
+    - Abuse handling
+    - ...
+  - ...
+- Reference
+  - IPFS Core API
+  - HTTP API
+  - go-ipfs packages
+    - go-ipfs-api
+    - go-ipfs-gateway
+    - ...
+  - js-ipfs packages
+    - js-ipfs-api
+    - js-ipfs-browser
+    - ...
+- Tools
+  - ipfs-pack
+  - ipfs-cluster
+  - ipget
+  - gx
+  - iptb
+  - fs-repo-migrations
+- Development
+  - Testing
+  - ...
+
+
+# IPFS Pages
+
+Inputs:
+- Github org or repo webhooks
+  - react on commit-push
+- Commit tree .webhook.yml
+
+Outputs:
+- IPFS DAG with rendered page, and log output
+- Status API
+- Pinning
+- DNS record updates
+- Trigger rebuild of targets which mount the current target (this is why we scan for repos)
+
+```yml
+---
+# libp2p/docs/.webhook.yml
+commands:
+  master:
+    - make build
+  docs-ng:
+    - make build
+mounts:
+  - src: github.com/libp2p/js-libp2p-*
+    dest: js/*
+  - src: github.com/libp2p/go-libp2p-*
+    dest: go/*
+  - src: github.com/libp2p/go-*-transport
+    dest: go/go-*-transport
+  - src: github.com/openbazaar
+    dest: go/go-onion-transport
+dnslinks:
+  master: /ipns/docs.libp2p.io
+  docs-ng: /ipns/docs-ng.libp2p.io
+
+---
+# libp2p/website/.webhook.yml
+
+commands:
+  master:
+    - make build
+dnslinks:
+  master: /ipns/libp2p.io
+```
+
+- mounts:
+  - these also act as triggers
+  - when a build of the referenced repo succeeds, we update the mount
+    - if we can receive webhooks for it
+  - foo-* means all repos starting in foo- with .webhook.yml in their default branch
+  - we scan for repos at startup, then handle repo.created/deleted webhooks
+- dnslinks:
+  - branch uses the current repo's named branch, and the same-named branches (or default branches) of mounted repos
+
+
+
+---
+
+pagesworkspace/
+  builds/
+    COMMIT_ID_1/
+      tree/
+        ...
+      build.log
+      cid
+    COMMIT_ID_N/
+      ...
+  repos/
+    github.com/
+      multiformats/
+        website/
+          .git
+
+> pagebuild -q github.com/multiformats/website COMMIT_ID
+CID
---
+++ pagebuild/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	// "fmt"
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+const (
+	buildsDir    = "builds"
+	reposDir     = "repos"
+	settingsFile = ".ipfspages.yml"
+)
+
+type Settings struct {
+	Commands []string
+	Output   string
+	Dnslinks map[string]string
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("usage: pagebuild github.com/ORG/REPO COMMIT")
+	}
+	rparts := strings.Split(os.Args[1], "/")
+	if len(rparts) != 3 {
+		log.Fatal("usage: pagebuild github.com/ORG/REPO COMMIT")
+	}
+
+	// owner := rparts[1]
+	// repo := rparts[2]
+	commit := os.Args[2]
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// repoDir := filepath.Join(cwd, reposDir, owner, repo)
+	// repoURL := fmt.Sprintf("git@github.com:%s/%s.git", owner, repo)
+	// buildDir := filepath.Join(cwd, buildsDir, commit)
+	treeDir := filepath.Join(cwd, buildsDir, commit, "tree")
+
+	// if err := cloneRepo(repoDir, repoURL); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// if err := cloneBuild(buildDir, repoDir, repoURL, commit); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// commitRelevant, err := exists(filepath.Join(treeDir, settingsFile))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if !commitRelevant {
+	// 	log.Printf("ignoring github.com/%s/%s %s", owner, repo, commit)
+	// 	if err := os.RemoveAll(buildDir); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	os.Exit(0)
+	// }
+
+	settings, err := parseSettingsFile(filepath.Join(treeDir, settingsFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h, err := build(treeDir, settings)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO:
+	// - ipfs files cp $h /github.com/ipfs/website
+	// - ipfs files publish
+	log.Printf("https://ipfs.io/ipfs/%s", h)
+}
+
+func build(treeDir string, settings *Settings) (string, error) {
+	for _, cmd := range settings.Commands {
+		if err := command(treeDir, "bash", "-c", cmd); err != nil {
+			return "", err
+		}
+	}
+
+	args := []string{"add", "-Q", "-r", filepath.Join(treeDir, settings.Output)}
+	log.Printf("> ipfs %s", strings.Join(args, " "))
+	c := exec.Command("ipfs", args...)
+	buf := new(bytes.Buffer)
+	c.Stdout = buf
+	c.Stderr = os.Stderr
+	if err := c.Run(); err != nil {
+		return "", nil
+	}
+
+	return buf.String(), nil
+}
+
+func parseSettingsFile(path string) (*Settings, error) {
+	yml, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var settings Settings
+	return &settings, yaml.Unmarshal(yml, &settings)
+}
+
+func cloneRepo(repoDir, repoURL string) error {
+	if err := os.MkdirAll(filepath.Join(repoDir, ".."), 0755); err != nil {
+		return err
+	}
+
+	repoexists, err := exists(repoDir)
+	if err != nil {
+		return err
+	}
+	if !repoexists {
+		if err = command("./", "git", "clone", "--bare", repoURL, repoDir); err != nil {
+			return err
+		}
+	}
+
+	if err = command(repoDir, "git", "fetch", "--all"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func cloneBuild(buildDir, repoDir, repoURL, commit string) error {
+	if err := os.RemoveAll(buildDir); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(buildDir, 0755); err != nil {
+		return err
+	}
+
+	treeDir := filepath.Join(buildDir, "tree")
+
+	if err := command(buildDir, "git", "clone", "--reference", repoDir, repoURL, treeDir); err != nil {
+		return err
+	}
+
+	if err := command(treeDir, "git", "checkout", "-q", commit); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func command(dir string, cmd string, args ...string) error {
+	log.Printf("> %s %s (cwd=%s)", cmd, strings.Join(args, " "), dir)
+	c := exec.Command(cmd, args...)
+	c.Dir = dir
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	return c.Run()
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
